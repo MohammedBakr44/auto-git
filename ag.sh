9ef063c3fc9fcfede4a424acc3a345dc6e05bae7
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2020-01-27 18:57:10 +0200

    testing

--- ag.sh
+++ ag.sh
@@ -12,6 +12,8 @@ read -p 'Enter desired remote branch: ' push
 
 if [ -z $push ] ; then push="origin master" ; fi
 
-read -p 'Force update?[Y/n] default is no' force
+read -p 'Force update?[Y/n] default is no: ' force
 
 if [ -z $force ] ; then git push $push else git push $push --force ; fi
+
+read
